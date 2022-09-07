ef40b1c6d4d6146ea604c2a58fd2767e44da3647
anniech1 <ach577@nyu.edu>
2022-09-07 03:48:07 -0400

    fixed email matching issue and added router.use functions in routes index files

---
+++ models/Thought.js
@@ -0,0 +1,68 @@
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
+
+const thoughtsSchema = new Schema(
+    {
+        thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        //Use a getter method to format the timestamp on query here? mongoose timestamp
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    },
+    username: {
+        type: String,
+        required: true
+    },
+
+    reactions: [reactionsSchema]
+    },
+    {
+        toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+    }
+)
+
+//not a seperate file but part of this one
+const reactionsSchema = new Schema(
+    {
+        reactionId: {
+        type: Schema.Types.ObjectId,
+        default: ()=> new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    }
+    },
+    {toJSON: {
+        getters: true
+    } 
+    }
+);
+
+thoughtsSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thoughts = model('Thoughts', thoughtsSchema);
+
+module.exports = Thoughts;
